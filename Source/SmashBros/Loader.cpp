722a2126ed67b755d7605d70fd7e66e03c4abf3d
Alekemon <42167718+Alekemon@users.noreply.github.com>
2022-01-03 11:38:39 +0100

    Added world tournament

--- Source/SmashBros/Loader.cpp
+++ Source/SmashBros/Loader.cpp
@@ -14,6 +14,7 @@
 #include "Game/Stages/FinalDestinationBrawl.h"
 #include "Game/Stages/HyruleTemple.h"
 #include "Game/Stages/Corneria.h"
+#include "Game/Stages/WorldTournament.h"
 
 #ifndef SMASHBROS_SCRIPT_DISABLE
 #include "../ScriptModule/ScriptManager.h"
@@ -341,6 +342,9 @@ namespace SmashBros
 
 			case Global::STAGE_CORNERIA:
 			return "corneria.png";
+			
+			case Global::STAGE_WORLDTOURNAMENT:
+			return "worldtournament.png";
 		}
 	}
 	
@@ -370,6 +374,9 @@ namespace SmashBros
 
 			case Global::STAGE_CORNERIA:
 			return new Corneria(x1,y1);
+				
+			case Global::STAGE_WORLDTOURNAMENT:
+			return new WorldTournament(x1,y1);
 			
 			default:
 #ifndef SMASHBROS_SCRIPT_DISABLE
